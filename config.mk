3e900a991392da83c1185171f072a5ac5f56707f
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2022-12-18 00:21:28 +0200

    Update translation

--- config.mk
+++ config.mk
@@ -2,4 +2,5 @@ DEVICE_PACKAGE_OVERLAYS += vendor/translations/overlay
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/translations/overlay
 
 PRODUCT_PACKAGES += \
-    crDroidMusicOverlay
+    crDroidMusicOverlay \
+    OPSoundTunerOverlay
